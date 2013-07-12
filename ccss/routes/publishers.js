cc1edad51c4ea80ce309e1a2e06a7be39cc6b4f0
Walt Grata <wegrata@gmail.com>
2013-07-12 09:36:50 -0400

    publisher no longer formats data in node, all happens in couchdb list functions

--- ccss/routes/publishers.js
+++ ccss/routes/publishers.js
@@ -1,5 +1,8 @@
 var couchdb = require('couchdb-api');
+var http = require('http');
+var querystring = require('querystring');
 var underscore = require('underscore');
+var url = require('url');
 var server       = couchdb.srv(process.env.COUCHDB || 'localhost', 5984, false, true);
 var db           = server.db('lr-data');
 var publisherView      = db.ddoc('publisher_view').view('publishers');
@@ -23,50 +26,65 @@ response.end("Not Found");
 }
 
 var pageSize = 25;
+function getView(gov){
+    if (gov){
+        return '/_design/publisher_view/_list/root/gov-publishers';
+    }else{
+        return '/_design/publisher_view/_list/root/publishers';
+    }    	
+}
+function serveView(response, view, key, page, includeDocs){
+    var urlParts = url.parse(db.url);
+    var opts = {
+        host: urlParts.hostname,
+        port: urlParts.port,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        protocol: urlParts.protocol,        
+        method: "GET"
+    };
+    opts.path = urlParts.path + view;
+
+    var data = {
+            stale: "update_after",
+            skip: page * pageSize,
+            limit: pageSize,
+            group: true,
+            inclusive_end: true,
+    };
+    if (key){
+    	data.key = JSON.stringify(key);
+    }    
+    if (includeDocs){
+      data.include_docs = true;
+      data.reduce = false;
+      data.group = false;
+    }    
+    opts.path = (opts.path + '?' + querystring.stringify(data));
+    response.writeHead(200, {"Content-Type": "application/json",
+                             "Access-Control-Allow-Origin": "*",
+                             "Access-Control-Allow-Methods": "GET",
+                             "Access-Control-Allow-Headers": "*"  });
+    var req = http.request(opts, function(res) {
+        res.pipe(response, {end: true});
+    });
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+    });
+    req.end();
+}
 exports.publishers = function(req, res){
 	var page = req.query.page || 0;
-	var view = req.query.gov ? govPublisherView : publisherView;
-	view.query({group: true, stale: "update_after", limit: pageSize, skip: page * pageSize,  inclusive_end: true}, function(err, results){
-		if(err){
-			console.log(err);
-			writeNotFound(res);
-		}else{
-			writeSuccess(underscore.map(results.rows, function(item){
-				return item.value;
-			}), res);
-		}
-	});
+	serveView(res, getView(req.query.gov), null, page);
 }
 exports.filteredPublishers = function(req, res){	
 	var page = req.query.page || 0;
 	var key = req.params.letter;
-	alphabetView.query({group: true, stale: "update_after", key: key, limit: pageSize, skip: page * pageSize,  inclusive_end: true}, function(err, results){
-		console.log(key);
-		if(err){
-			console.log(err);
-			writeNotFound(res);
-		}else{
-			writeSuccess(results.rows[0].value, res);
-		}
-	});
+	serveView(res, "/_design/publisher_view/_list/child/firstLetter", key, page);
 }
 exports.publisher = function(req, res){
 	var pub = req.params.pub;
 	var page = req.query.page || 0;
-	publisherView.query({reduce: false, key: pub.toLowerCase(), include_docs: true, stale: "update_after", limit: pageSize, skip: page * pageSize,  inclusive_end: true}, function(err, results){
-		if(err){
-			writeNotFound(res);
-		}else{
-			writeSuccess(underscore.map(results.rows, function(item){
-				var doc = item.doc;
-				if(doc._attachments){
-					delete doc._attachments;
-					doc.hasScreenshot = true;
-				}else{
-					doc.hasScreenshot = false;
-				}
-				return item.doc;
-			}), res);
-		}
-	});	
+  serveView(res, '/_design/publisher_view/_list/root/publishers', pub.toLowerCase(), page, true);
 }
